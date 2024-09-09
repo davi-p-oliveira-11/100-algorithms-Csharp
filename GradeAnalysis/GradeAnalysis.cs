318186740618fca407a49925a5295098849a2973
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-09 12:23:21 -0300

    updated algo 19

--- GradeAnalysis/GradeAnalysis.cs
+++ GradeAnalysis/GradeAnalysis.cs
@@ -1,3 +1,11 @@
+/* 
+19) Create an algorithm that reads a student's name and two grades, 
+calculates their average, and displays it on the screen. 
+In the end, analyze the average and show whether 
+the student had good performance (if it was above the average of 7.0).
+*/
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;

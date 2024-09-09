f567027f013ec8cf8204a2e4a5389aabb95a155a
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-09 12:25:05 -0300

    updated algo 17

--- SpeedMonitor/SpeedMonitor.cs
+++ SpeedMonitor/SpeedMonitor.cs
@@ -1,3 +1,10 @@
+/* 
+17) Write a program that asks for the speed of a car. 
+If it exceeds 80 km/h, display a message saying that the user has been fined. 
+In this case, display the amount of the fine, charging R$5 for each km above the allowed speed.
+*/
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
